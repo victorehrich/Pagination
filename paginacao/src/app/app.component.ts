44fb2aadb85c798e7ba2046b6f0ac296dfbbc414
unknown <victorehrich@gmail.com>
2021-07-15 02:15:43 -0300

    some progress, first ideias of prev and next button with value

--- paginacao/src/app/app.component.ts
+++ paginacao/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'paginacao';
+
+  criador:string[] = [];
+  itensTotal = function(z:number){
+    let criador:string[] = [];
+    for(let i = 0; i<12; i++){
+      criador[i] = "item "+(i+1);
+    }
+    return criador[z];
+  }
+  itensShow(i:number, tamanhoShow:number){
+    let shower:string[] = [] ;
+    
+    for(i; i < tamanhoShow; i++){
+      shower[i]= this.itensTotal(i);
+    }
+    console.log(shower,tamanhoShow)
+    tamanhoShow += tamanhoShow;
+    return shower;
+  }
+
+  nextItem(){
+    this.x++;
+  }
+  prevItem(){
+    this.x--;
+  }
+  x = 1;
+  valueReturner(){
+    return {value: this.x}
+  }
 }
+
+
+
